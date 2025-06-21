567251e94edad0cab3e92ae25f70b8c34d409fe9
Rajan <ra580jan@gmail.com>
2025-06-21 12:02:08 +0530

    update server

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
 const authRouter = require('./routes/authRouter');
 require('dotenv').config();
 require('./models/dbConnection');
 const cors = require('cors');
-app.get('/', (req, res) => {
-    res.send('Welcome to the backend server!');
-})
+const serverless = require('serverless-http'); // <-- ADD THIS
 
 app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the backend server!');
+});
 
 app.use('/auth', authRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+// No app.listen() for Vercel
+module.exports = serverless(app); // <-- EXPORT like this
