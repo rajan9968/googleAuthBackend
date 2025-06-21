f6ae476eb4f7ca3a6907aff8a96d7ec60f7b540c
Rajan <ra580jan@gmail.com>
2025-06-21 13:37:49 +0530

    Fix: Vercel 404 issue with correct serverless export

--- utils/googleConfig.js
+++ utils/googleConfig.js
@@ -5,5 +5,5 @@ require('dotenv').config();
 exports.oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    'postmessage'
+    process.env.REDIRECT_URI
 );
